e59dfbb27ffce42aafcb921b82e531a9a2f4717d
Alexander Kohan <kohan.alexander@hotmail.com>
2016-07-08 21:16:36 +0900

    init

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "kayur"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/net/kayur/nlp/Corpus.scala
@@ -0,0 +1,11 @@
+package net.kayur.nlp
+
+/**
+  * Created by alex on 7/8/16.
+  */
+class Corpus {
+
+}
+
+class Document(val id: Int, val category: Int, val words: Map[Int, Int])
+
